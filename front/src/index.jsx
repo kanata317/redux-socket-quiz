8db69c082c23f69ceb014fa4fb9435ac503f3fe2
kanata317 <y_tanaka317@yahoo.co.jp>
2016-07-16 14:45:49 +0900

    arrange dir architecture

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import injectTapEventPlugin from "react-tap-event-plugin";
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
-import App from '../containers/App';
-import configureStore from '../store/configureStore';
+import App from './containers/App';
+import configureStore from './store/configureStore';
 
 //Needed for React Developer Tools
 window.React = React;
@@ -19,8 +19,6 @@ injectTapEventPlugin();
 const store = configureStore();
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+    <Provider store={store}>
+    <App/>
+</Provider>, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
 module.exports = {
-  context: __dirname,
+  context: __dirname + "/front/src",
   entry: {
-    jsx: "./src/index.jsx",
-    css: "./src/main.css",
-    html: "./src/index.html",
+    jsx: "./index.jsx",
+    css: "./main.css",
+    html: "./index.html"
   },
 
   output: {
-    path: __dirname + "/static",
-    filename: "bundle.js",
+    path: __dirname + "/public",
+    filename: "bundle.js"
   },
   module: {
     preLoaders: [
         //Eslint loader
-      { test: /\.jsx?$/, exclude: /node_modules/, loader: "eslint-loader"},
+      { test: /\.jsx?$/, exclude: /node_modules/, loader: "eslint-loader"}
     ],
     loaders: [
       { test: /\.html$/, loader: "file?name=[name].[ext]" },
       { test: /\.css$/, loader: "file?name=[name].[ext]" },
-      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ["react-hot","babel-loader"]},
-    ],
+      { test: /\.jsx?$/, exclude: /node_modules/, loaders: ["react-hot","babel-loader"]}
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   eslint: {
     configFile: './.eslintrc'
-  },
+  }
 };
